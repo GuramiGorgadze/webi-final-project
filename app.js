d972f138afef6ae17ee927b8d9a54525edbab28f
Gurami <g.gorgadze111@gmail.com>
2025-04-17 19:15:21 +0400

    Change app.js

--- app.js
+++ app.js
@@ -3,24 +3,35 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(session({
+  secret: 'IDK',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: true }
+}));
+
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', loginRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
